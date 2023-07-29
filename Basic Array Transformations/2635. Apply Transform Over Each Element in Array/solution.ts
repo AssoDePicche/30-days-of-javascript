0b9eb73b140a530a91cead359f1043533aadce16
AssoDePicche <samuelprado730@gmail.com>
2023-07-29 10:42:59 -0300

    feat: 2635. apply transform over each element in array

---
+++ Basic Array Transformations/2635. Apply Transform Over Each Element in Array/solution.ts
@@ -0,0 +1,10 @@
+const map = function (
+  arr: number[],
+  fn: (n: number, i: number) => number
+): number[] {
+  for (let index = 0; index < arr.length; ++index) {
+    arr[index] = fn(arr[index], index);
+  }
+
+  return arr;
+};
