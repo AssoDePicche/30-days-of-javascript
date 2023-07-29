f0478d5eda808fd83e08cc7078677d72e89d1c2a
AssoDePicche <samuelprado730@gmail.com>
2023-07-29 11:14:47 -0300

    feat: 2626. array reduce transformation

---
+++ Basic Array Transformations/2626. Array Reduce Transformation/solution.ts
@@ -0,0 +1,11 @@
+type Fn = (accum: number, curr: number) => number;
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+  if (nums.length === 0) return init;
+
+  for (let index: number = 0; index < nums.length; ++index) {
+    init = fn(init, nums[index]);
+  }
+
+  return init;
+}
