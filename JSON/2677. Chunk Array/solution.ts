25bb53246c247263952c27288e822a79759a0eab
AssoDePicche <samuelprado730@gmail.com>
2023-08-05 23:43:03 -0300

    feat: 2677. chunk array

---
+++ JSON/2677. Chunk Array/solution.ts
@@ -0,0 +1,19 @@
+const chunk = function (arr: any[], size: number): any[][] {
+  let chunkArray: any[][] = [];
+
+  let chunkIndex: number = 0;
+
+  const length: number = arr.length;
+
+  for (
+    let currentChunk: number = 0;
+    currentChunk < length;
+    currentChunk += size
+  ) {
+    chunkArray[chunkIndex] = arr.slice(currentChunk, currentChunk + size);
+
+    ++chunkIndex;
+  }
+
+  return chunkArray;
+};
